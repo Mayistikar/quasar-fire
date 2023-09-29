7bfa78798b576d9d2eb80575edb53559eb2d8942
Mayistikar <andersonrodriguezce@gmail.com>
2023-09-29 12:07:55 -0500

    solving level 1

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,91 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"gonum.org/v1/gonum/mat"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	fmt.Println(GetLocation(100, 115.5, 142.7))
+	fmt.Println(GetMessage(
+		[]string{"", "este", "es", "un", "mensaje"},
+		[]string{"este", "", "un", "mensaje"},
+		[]string{"", "", "es", "", "mensaje"},
+	))
+}
+
+// GetLocation calculates the location of a ship returning
+// the x and y coordinates of location making triangulation
+// of distances from 3 known point
+func GetLocation(distances ...float32) (float32, float32) {
+
+	// -- Satellites locations -- //
+
+	// Kenobi
+	x1, y1 := -500.0, -200.0
+	// Skywalker
+	x2, y2 := 100.0, -100.0
+	// Sato
+	x3, y3 := 500.0, 100.0
+
+	// -- Distances from satellites -- //
+
+	// Distances from satellites
+	d1 := float64(distances[0])
+	d2 := float64(distances[1])
+	d3 := float64(distances[2])
+
+	// -- Calculate the location -- //
+	// To calculate the location of ship we need to solve the system of linear equations
+
+	// Making matrix of coefficients A
+	A := mat.NewDense(3, 2, []float64{2 * (x1 - x3), 2 * (y1 - y3), 2 * (x2 - x3), 2 * (y2 - y3), 2 * (x1 - x2), 2 * (y1 - y2)})
+	// Making vector of constants b
+	b := mat.NewVecDense(3, []float64{d1*d1 - d3*d3 - x1*x1 + x3*x3 - y1*y1 + y3*y3, d2*d2 - d3*d3 - x2*x2 + x3*x3 - y2*y2 + y3*y3, d1*d1 - d2*d2 - x1*x1 + x2*x2 - y1*y1 + y2*y2})
+
+	// Solve the system of linear equations
+	var xVec mat.VecDense
+	if err := xVec.SolveVec(A, b); err != nil {
+		fmt.Println("Error solving the system of linear equations")
+		return 0, 0
+	}
+
+	// Return the coordinates
+	return float32(xVec.AtVec(0)), float32(xVec.AtVec(1))
+}
+
+// GetMessage returns the message from the ship comparing the messages
+// from the satellites and returning the message with the missing words
+func GetMessage(messages ...[]string) string {
+	var reverseMessage []string
+
+	for index, message := range messages {
+		counter := 0
+
+		// Iterate over the slice from back to front to avoid the gaps in the message
+		for i := len(message) - 1; i >= 0; i-- {
+			if index == 0 {
+				reverseMessage = append(reverseMessage, message[i])
+				continue
+			}
+
+			// If the message is not empty and the message is different from the message in reverseMessage
+			if message[i] != "" && message[i] != reverseMessage[counter] {
+				reverseMessage[counter] = message[i]
+			}
+
+			counter++
+		}
+	}
+
+	var decodedMessage string
+	// Iterate over the slice from back to front to organize the message
+	for i := range reverseMessage {
+		index := len(reverseMessage) - 1 - i
+		decodedMessage += reverseMessage[index] + " "
+	}
+
+	// Remove the spaces from the message
+	return strings.Trim(decodedMessage, " ")
 }
---
+++ go.mod
@@ -0,0 +1,7 @@
+module quasar.fire.com
+
+go 1.20
+
+require gonum.org/v1/gonum v0.14.0
+
+require golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
